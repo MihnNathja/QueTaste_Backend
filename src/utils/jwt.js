7cc0585784ad8b9851050f2acba01fcf05a4403e
Huynh-Minh-Man-04042004 <nsndman0404@gmail.com>
2025-09-22 05:48:52 +0700

    Fix conflict when merging main to dev_man

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,9 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (userId) => {
-    const accessToken = jwt.sign({id: userId}, process.env.JWT_SECRET, {expiresIn: "15m"});
-    const refreshToken = jwt.sign({id: userId}, process.env.JWT_REFRESH_SECRET, {expiresIn: "7d"});
-    return {accessToken, refreshToken};
 const generateToken = (user) => {
     const payload = {
         id: user._id,
