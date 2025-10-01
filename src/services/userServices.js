eb2ba7773f9258a151d3e9ff9d219c6f590369d3
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2025-10-01 14:05:20 +0700

    Update profile: update model User.js, userServices.js for upgrade selecting user address

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -6,14 +6,22 @@ const getProfile = async (userId) => {
 };
 
 const updateProfile = async (userId, body, file) => {
-  const { fullName, phone, address, dateOfBirth, gender } = body;
+  const { fullName, phone, shippingAddress, dateOfBirth, gender } = body;
   const updateData = {};
 
   if (fullName) updateData["personalInfo.fullName"] = fullName;
   if (phone) updateData["personalInfo.phone"] = phone;
-  if (address) updateData["personalInfo.address"] = address;
   if (dateOfBirth) updateData["personalInfo.dateOfBirth"] = dateOfBirth;
   if (gender) updateData["personalInfo.gender"] = gender;
+
+  if (shippingAddress) {
+    try {
+      const parsedAddress = JSON.parse(shippingAddress);
+      updateData["personalInfo.shippingAddress"] = parsedAddress;
+    } catch (err) {
+      console.error("❌ Lỗi parse shippingAddress:", err);
+    }
+  }
 
   if (file) {
     const result = await cloudinary.uploader.upload(file.path, {
@@ -25,9 +33,11 @@ const updateProfile = async (userId, body, file) => {
     updateData.avatar = result.secure_url;
   }
 
-  return await User.findByIdAndUpdate(userId, { $set: updateData }, { new: true }).select(
-    "-password -__v"
-  );
+  return await User.findByIdAndUpdate(
+    userId,
+    { $set: updateData },
+    { new: true }
+  ).select("-password -__v");
 };
 
 const searchUsers = async (keyword, role) => {
@@ -40,7 +50,9 @@ const searchUsers = async (keyword, role) => {
     filter.role = role;
   }
 
-  return await User.find(filter).select("_id avatar role personalInfo.fullName");
+  return await User.find(filter).select(
+    "_id avatar role personalInfo.fullName"
+  );
 };
 
 module.exports = {
