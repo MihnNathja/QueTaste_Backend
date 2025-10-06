af2309f20c1a9ecf1579af0ab1fbb82dcd341b85
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2025-10-06 10:44:33 +0700

    Update Order Management, Review Management

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -4,29 +4,29 @@ const { notifyUser, notifyAdmins } = require("../services/notificationService");
 
 // POST /api/order/checkout
 exports.checkout = async (req, res) => {
-    try {
-        const order = await OrderService.checkout(req.user.id, req.body);
+  try {
+    const order = await OrderService.checkout(req.user.id, req.body);
 
-        await notifyUser(req.user.id, {
-          type: "order",
-          message: `Đơn hàng #${order._id} của bạn đã được tạo thành công`,
-          link: `/orders/${order._id}`,
-          sendEmail: true, // gửi email
-          priority: "high"
-        });
+    await notifyUser(req.user.id, {
+      type: "order",
+      message: `Đơn hàng #${order._id} của bạn đã được tạo thành công`,
+      link: `/orders/${order._id}`,
+      sendEmail: true, // gửi email
+      priority: "high",
+    });
 
-        await notifyAdmins({
-          type: "order",
-          message: `Có đơn hàng mới #${order._id} từ ${req.user.id}`,
-          link: `/admin/orders/${order._id}`,
-          priority: "high",
-        });
+    await notifyAdmins({
+      type: "order",
+      message: `Có đơn hàng mới #${order._id} từ ${req.user.id}`,
+      link: `/admin/orders/${order._id}`,
+      priority: "high",
+    });
 
-        return sendResponse(res, 201, true, "Order created successfully", order);
-    } catch (err) {
-        console.error("Error in checkout controller:", err.message);
-        return sendResponse(res, 400, false, err.message);
-    }
+    return sendResponse(res, 201, true, "Order created successfully", order);
+  } catch (err) {
+    console.error("Error in checkout controller:", err.message);
+    return sendResponse(res, 400, false, err.message);
+  }
 };
 
 exports.getMyOrders = async (req, res) => {
@@ -53,21 +53,21 @@ exports.getMyOrders = async (req, res) => {
 exports.cancelOrder = async (req, res) => {
   try {
     const userId = req.user.id;
-    const {orderId} = req.params;
+    const { orderId } = req.params;
     const order = await OrderService.cancelOrder(userId, orderId);
 
     await notifyUser(req.user.id, {
       type: "order",
       message: `Đơn hàng #${order._id} đã được hủy`,
       link: `/orders/${order._id}`,
-      priority: "high"
+      priority: "high",
     });
 
     await notifyAdmins({
       type: "order",
       message: `Khách ${req.user.id} vừa hủy đơn hàng #${order._id}`,
       link: `/admin/orders/${order._id}`,
-      priority: "high"
+      priority: "high",
     });
 
     return sendResponse(res, 200, true, "Order cancelled successfully", order);
@@ -75,41 +75,58 @@ exports.cancelOrder = async (req, res) => {
     console.error("Error in cancelOrder controller:", err.message);
     return sendResponse(res, 400, false, err.message);
   }
-}
+};
 
 exports.requestCancelOrder = async (req, res) => {
   try {
     const userId = req.user.id;
     const { orderId } = req.params;
     const { reason } = req.body;
-    console.log("Requesting cancellation for order:", orderId, "by user:", userId, "with reason:", reason);
-    const order = await OrderService.requestCancelOrder(userId, orderId, reason);
-    return sendResponse(res, 200, true, "Order cancellation requested successfully", order);
+    console.log(
+      "Requesting cancellation for order:",
+      orderId,
+      "by user:",
+      userId,
+      "with reason:",
+      reason
+    );
+    const order = await OrderService.requestCancelOrder(
+      userId,
+      orderId,
+      reason
+    );
+    return sendResponse(
+      res,
+      200,
+      true,
+      "Order cancellation requested successfully",
+      order
+    );
   } catch (err) {
     console.error("Error in requestCancelOrder controller:", err.message);
     return sendResponse(res, 400, false, err.message);
   }
-}
+};
 
 // POST /api/order/momo/notify
 exports.momoNotify = async (req, res) => {
-    try {
-        const { orderId, resultCode } = req.body;
-        await OrderService.handleMomoNotify(orderId, resultCode);
-        return sendResponse(res, 200, true, "MoMo notification processed");
-    } catch (err) {
-        return sendResponse(res, 400, false, err.message);
-    }
+  try {
+    const { orderId, resultCode } = req.body;
+    await OrderService.handleMomoNotify(orderId, resultCode);
+    return sendResponse(res, 200, true, "MoMo notification processed");
+  } catch (err) {
+    return sendResponse(res, 400, false, err.message);
+  }
 };
 
 // POST /api/order/update-status
 exports.updateStatus = async (req, res) => {
   try {
-      const { orderId, resultCode } = req.body;
-      const order = await OrderService.updateStatus(orderId, resultCode);
-      return sendResponse(res, 200, true, "Order status updated", order);
+    const { orderId, resultCode } = req.body;
+    const order = await OrderService.updateStatus(orderId, resultCode);
+    return sendResponse(res, 200, true, "Order status updated", order);
   } catch (err) {
-      return sendResponse(res, 400, false, err.message);
+    return sendResponse(res, 400, false, err.message);
   }
 };
 
@@ -132,12 +149,12 @@ exports.getAllOrders = async (req, res) => {
   }
 };
 
-exports.acceptOrder = async (req, res) => {
+exports.confirmOrder = async (req, res) => {
   try {
-      const { orderId } = req.body;
-      const order = await OrderService.updateOrderStatus(orderId, "ACCEPT");
-      return sendResponse(res, 200, true, "Order status updated", order);
+    const { orderId } = req.params;
+    const order = await OrderService.updateOrderStatus(orderId, "confirmed");
+    return sendResponse(res, 200, true, "Order status updated", order);
   } catch (err) {
-      return sendResponse(res, 400, false, err.message);
+    return sendResponse(res, 400, false, err.message);
   }
-}
+};
--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const orderController = require("../controllers/orderController");
-const authMiddleware = require('../middleware/authMiddleware')
-const adminMiddleware = require("../middleware/adminMiddleware")
+const authMiddleware = require("../middleware/authMiddleware");
+const adminMiddleware = require("../middleware/adminMiddleware");
 
 const router = express.Router();
 
 router.post("/checkout", authMiddleware, orderController.checkout);
 router.get("/me", authMiddleware, orderController.getMyOrders);
 router.post("/cancel/:orderId", authMiddleware, orderController.cancelOrder);
-router.post("/request-cancel/:orderId", authMiddleware, orderController.requestCancelOrder);
+router.post(
+  "/request-cancel/:orderId",
+  authMiddleware,
+  orderController.requestCancelOrder
+);
 router.post("/momo/notify", orderController.momoNotify);
 router.post("/update-status", orderController.updateStatus);
+router.put("/confirm/:orderId", orderController.confirmOrder);
 
 //router.get("/get-all", authMiddleware, adminMiddleware, orderController.getAllOrders)
-router.get("/get-all", orderController.getAllOrders)
+router.get("/get-all", orderController.getAllOrders);
 
 module.exports = router;
